472b50b8080db47db83f3f403d30aa6667a503bf
MQL-IT <mengqingling@MQLing.local>
2017-03-05 13:32:07 +0800

    计算器

---
+++ Calculator/CalculatorBrain.swift
@@ -0,0 +1,83 @@
+//
+//  CalculatorBrain.swift
+//  Calculator
+//
+//  Created by MQL-IT on 2017/3/5.
+//  Copyright © 2017年 MIT. All rights reserved.
+//
+
+import Foundation
+
+
+func multiply(op1: Double, op2: Double) -> Double {
+    return op1 * op2
+}
+
+class CalculatorBrain {
+    private var accumulator = 0.0 // 要显示的计算结果
+    
+    func setOperand(operand: Double) {
+        accumulator = operand
+    
+    }
+    // 运算符表格扩展方便
+    var operations: Dictionary<String, Operation > = [
+        "π" : Operation.Constant(M_PI),
+        "e" : Operation.Constant(M_E),
+        "√" : Operation.UnaryOperation(sqrt),// sqrt,
+        "cos" : Operation.UnaryOperation(cos),
+        "×" : Operation.BinaryOperation({$0 * $1 }),  // ÷－＋
+        "-" : Operation.BinaryOperation({$0 - $1}),
+        "+" : Operation.BinaryOperation({$0 + $1}),
+        "÷" : Operation.BinaryOperation({$0 / $1}),
+        "=" : Operation.Equals
+    ]
+    
+    // 运算符类型的枚举
+    enum Operation {
+        case Constant(Double)
+        case UnaryOperation((Double) -> Double)
+        case BinaryOperation((Double, Double) -> Double)
+        case Equals
+    }
+    
+    // 计算
+    func performOperation(symbol: String) {
+        
+        guard let operation = operations[symbol] else {return}
+        switch operation {
+        case .Constant(let associatedConstantValue):
+            accumulator = associatedConstantValue
+        case .UnaryOperation(let function):
+            accumulator = function(accumulator)
+        case .BinaryOperation(let function):
+            executePendingBinaryOperation()
+            pending = PendingBinaryOperationInfo(binaryFunction: function, fistOprand: accumulator)
+        case .Equals:
+            executePendingBinaryOperation()
+        }
+    }
+    
+    private func executePendingBinaryOperation() {
+        if pending != nil {
+            accumulator = pending!.binaryFunction(pending!.fistOprand, accumulator)
+            pending = nil
+        }
+    }
+    
+    private var pending: PendingBinaryOperationInfo?
+    
+    // 存放二元计算类型和参数的结构体
+    struct PendingBinaryOperationInfo {
+        var binaryFunction: (Double, Double) -> Double
+        var fistOprand: Double
+        
+    }
+    
+    // 只读的计算结果
+    var result: Double {
+        get {
+            return accumulator
+        }
+    }
+}
---
+++ Calculator/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by MQL-IT on 2017/3/5.
+//  Copyright © 2017年 MIT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet private weak var display: UILabel!
+    
+    private var userIsInTheMiddleOfTyping = false
+    
+
+    @IBAction private func touchDigit(_ sender: UIButton) {
+        
+        guard let digit = sender.currentTitle else { return }
+        
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        } else {
+            display.text = digit
+        }
+        userIsInTheMiddleOfTyping = true
+        
+    }
+    
+    private var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
+    }
+
+    private var brain = CalculatorBrain()
+    @IBAction private func performOperation(_ sender: UIButton) {
+        if userIsInTheMiddleOfTyping {
+            brain.setOperand(operand: displayValue)
+        }
+        userIsInTheMiddleOfTyping = false
+        guard let mathematicalSymbol = sender.currentTitle else { return  }
+        
+        brain.performOperation(symbol: mathematicalSymbol)
+        displayValue = brain.result
+        
+    }
+    
+    
+    
+}
+
